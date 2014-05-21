8ab373d8bb02f02baa1b3e6431cabc1e527040a7
Vamsi Vidala <vamsi.vidala@forrent.com>
2014-05-21 17:07:58 -0400

    Adding logic to point to correct database host(not tested the change)

--- scripts/confirmGcDatabase.php
+++ scripts/confirmGcDatabase.php
@@ -1,7 +1,28 @@
 <?php
+	/**
+	 * Example Usage:
+	 * 		php confirmGcDatabase.php needle number_of_guestcards
+	 **/
+
 	$comments = (int)$argv[1];
 	$success = (int)$argv[2];
-	$conn = mssql_pconnect('beta_database', 'readonly', 'readonly') or die("Couldn't connect to SQL Server\n");
+
+	//Finding the server env
+	defined('APPLICATION_ENV') || define('APPLICATION_ENV', (getenv('ServerEnvironment') ? getenv('ServerEnvironment') : 'live'));
+	$databaseHost = 'live_database';
+	switch (APPLICATION_ENV) {
+		case 'dev':
+			$databaseHost = 'dev_database';
+			break;
+		case 'beta': 
+			$databaseHost = 'beta_database';
+			break;
+		default:
+			$databaseHost = 'live_database';
+			break;
+	}
+
+	$conn = mssql_pconnect($databaseHost, 'readonly', 'readonly') or die("Couldn't connect to SQL Server\n");
 	$connectDB = mssql_select_db('FRC', $conn)or die("Couldn't open database.\n");
 	$getGc = " ";
 	$query = "select count(*) gcs from dbo.guest_card (nolock) where  guest_card_date > dateadd(day,-2,getdate()) and Comments LIKE '%" .$comments."%'";
